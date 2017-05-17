b9886d8a8e9f21022aaecb3f6bc83d4a4f72b72b
Oscar-21 <austin.conder@outlook.com>
2017-05-17 11:18:04 -0400

    changed package.json

---
+++ app/components/Splash/index.js
@@ -0,0 +1,21 @@
+/**
+*
+* Splash
+*
+*/
+
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import FontIcon from 'material-ui/FontIcon';
+
+class Splash extends React.PureComponent {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Splash;
